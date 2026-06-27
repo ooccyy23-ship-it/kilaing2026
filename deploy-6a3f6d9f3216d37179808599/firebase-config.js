261921a6a1c3d41ad23289cfb7d943e549460244
ooccyy23-ship-it <ooccyy.23@gmail.com>
2026-06-27 15:35:13 +0800

    Delete deploy-6a3f6d9f3216d37179808599 directory

--- deploy-6a3f6d9f3216d37179808599/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyA4BfF-2fY-7QAvySHdW_P1Moer8q4LSRw",
-  authDomain: "kilaingtjaiyisu2026.firebaseapp.com",
-  projectId: "kilaingtjaiyisu2026",
-  storageBucket: "kilaingtjaiyisu2026.firebasestorage.app",
-  messagingSenderId: "842546681203",
-  appId: "1:842546681203:web:3e9f73c0e553ff6e3da106",
-  appCheckSiteKey: "6LdUVCgtAAAAAA-HhBb7YUASn2STYzIWuDs1OgRd"
-};
